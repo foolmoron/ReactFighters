083623fc4f0fb87eddf7932f627cc01365163254
Momin Khan <foolmoron@gmail.com>
2016-11-07 18:44:12 -0500

    Image names

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 loaders: [
-                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'file?hash=sha512&digest=hex&name=[name].[hash].[ext]',
                     'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
                 ],
                 include: [/sprites.*/],
