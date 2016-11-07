24cfba4697ec3b6990a057758ccf409188891b8b
Momin Khan <foolmoron@gmail.com>
2016-11-07 11:53:41 -0500

    webpack babel init

---
+++ scripts/game.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+class Game extends React.Component {
+    render() {
+        return <h1>Game test 2</h1>
+    }
+}
+
+ReactDOM.render(<Game/>, document.getElementById('root'));
