5fa3a398fd56ad3c9c927b324f4f8906014251cb
Momin Khan <foolmoron@gmail.com>
2016-11-10 16:21:00 -0500

    Direction remembering

--- scripts/reducers/characterReducer.js
+++ scripts/reducers/characterReducer.js
@@ -5,7 +5,7 @@ export default (state = {
 }, action, fullState) => {
     switch (action.type) {
         case UPDATE:
-            return Object.assign({}, state, { angle: Math.atan2(fullState.direction.y, fullState.direction.x) * 180 / Math.PI });
+            return Object.assign({}, state, { angle: Math.atan2(fullState.direction.lastY, fullState.direction.lastX) * 180 / Math.PI });
         default: 
             return state;
     }
--- scripts/reducers/directionReducer.js
+++ scripts/reducers/directionReducer.js
@@ -9,26 +9,33 @@ export default (state = {
     M: false,
     x: 0,
     y: 0,
+    lastX: 0,
+    lastY: 0,
 }, action, fullState) => {
     switch (action.type) {
         case KEY_UP:
         case KEY_DOWN:
-            var { U, D, L, R, x, y } = state;
+            var { U, D, L, R, x, y, lastX, lastY } = state;
             // direction bools
             if (action.key == 'KeyW' || action.key == 'ArrowUp') { U = action.type == KEY_DOWN; }
             if (action.key == 'KeyS' || action.key == 'ArrowDown') { D = action.type == KEY_DOWN; }
             if (action.key == 'KeyA' || action.key == 'ArrowLeft') { L = action.type == KEY_DOWN; }
             if (action.key == 'KeyD' || action.key == 'ArrowRight') { R = action.type == KEY_DOWN; }
             // direction vector2
-            [x, y] = normalize([L ? -1 : R ? 1 : 0, U ? -1 : D ? 1 : 0]);
+            var [x, y] = normalize([L ? -1 : R ? 1 : 0, U ? -1 : D ? 1 : 0]);
+            // last non-zero direction
+            if (x != 0 || y != 0) {
+                lastX = x;
+                lastY = y;
+            }
             // return new state
-            return Object.assign({}, state, { U, D, L, R, x, y });
+            return Object.assign({}, state, { U, D, L, R, x, y, lastX, lastY });
         case MOUSE_UP:
             // disable movement
             return Object.assign({}, state, { x: 0, y: 0, M: false });
         case MOUSE_DOWN:
         case MOUSE_MOVE:
-            var { x, y, M } = state;
+            var { x, y, lastX, lastY, M } = state;
             // mouse bool
             M = M || action.type == MOUSE_DOWN;
             // direction is relative to center of viewport
@@ -37,8 +44,13 @@ export default (state = {
                 var h = window.innerHeight;
                 [x, y] = normalize([action.mouseX - (w/2), action.mouseY - (h/2)]);
             }
+            // last non-zero direction
+            if (x != 0 || y != 0) {
+                lastX = x;
+                lastY = y;
+            }
             // return new state
-            return Object.assign({}, state, { x, y, M });
+            return Object.assign({}, state, { x, y, lastX, lastY, M });
         default: 
             return state;
     }
